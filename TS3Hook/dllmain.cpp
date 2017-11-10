d8d77bf4e207b58b283a3ce972418f06d34e75d8
Splamy <splamyn@gmail.com>
2017-11-10 06:42:30 +0100

    Working 1.0

--- TS3Hook/dllmain.cpp
+++ TS3Hook/dllmain.cpp
@@ -4,20 +4,31 @@
 #include <Psapi.h>
 #include "PatchTools.h"
 
-#define REBASE(A) (A  + dwModuleBase)
-
 BOOL APIENTRY DllMain(HMODULE hModule, const DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-	DWORD dwModuleBase;
-	DWORD modSize;
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		printf("-==== TS3HOOK 1.0 ====-\n");
+		printf("-= Written by Splamy =-\n");
 
-		const auto match = FindPattern(L"ts3client_win32.exe", MASK1, PATT1);
-		if (match != NULL)
+		const auto match_in_1 = FindPattern(L"ts3client_win32.exe", MASK_IN_1, PATT_IN_1);
+		if (match_in_1 != NULL)
+			printf("> Found PKGIN1: %X\n", match_in_1);
+
+		const auto match_out_1 = FindPattern(L"ts3client_win32.exe", MASK_OUT_1, PATT_OUT_1);
+		if (match_out_1 != NULL)
+			printf("> Found PKGOUT1: %X\n", match_out_1);
+
+		if (match_in_1 != NULL && match_out_1 != NULL)
 		{
-			printf("Found packet dispatch: %X\n", match);
+			const DWORD OFFS_IN_1 = 13;
+			packet_in_hook_return = match_in_1 + OFFS_IN_1 + 5;
+			MakeJMP((PBYTE)(match_in_1 + OFFS_IN_1), packet_in_hook1, 5);
+
+			const DWORD OFFS_OUT_1 = 33;
+			packet_out_hook_return = match_out_1 + OFFS_OUT_1 + 8;
+			MakeJMP((PBYTE)(match_out_1 + OFFS_OUT_1), packet_out_hook1, 8);
 		}
 		else
 		{
@@ -25,11 +36,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, const DWORD ul_reason_for_call, LPVOID lp
 			return FALSE;
 		}
 
-		in_packet_hook_return = match + OFFS1 + 5;
-		MakeJMP((PBYTE)(match + OFFS1), in_packet_hook, 5);
-
-
-		CreateThread(nullptr, NULL, (LPTHREAD_START_ROUTINE)IdleLoop, nullptr, NULL, nullptr);
+		CreateThread(nullptr, NULL, (LPTHREAD_START_ROUTINE)idle_loop, nullptr, NULL, nullptr);
 		break;
 
 		//case DLL_THREAD_ATTACH:
@@ -40,32 +47,60 @@ BOOL APIENTRY DllMain(HMODULE hModule, const DWORD ul_reason_for_call, LPVOID lp
 	return TRUE;
 }
 
-const char* print_in_format = "[IN] %.*s\n";
-
-void __declspec(naked) in_packet_hook()
+const char* print_in_format1 = "[IN] %.*s\n";
+void __declspec(naked) packet_in_hook1()
 {
 	__asm
 	{
+		// +11
+
 		PUSHAD
-		MOV eax, [esi+4]
+		MOV eax, [esi + 4]
 		ADD eax, 11
-		PUSH eax
-		MOV ecx, [esi+8]
+		PUSH eax // str
+		MOV ecx, [esi + 8]
 		SUB ecx, 11
-		PUSH ecx
-		PUSH print_in_format
+		PUSH ecx // len
+		PUSH print_in_format1
 		CALL printf
 		ADD esp, 12
 		POPAD
 
+		// overwritten
 		PUSH edi
 		PUSH esi
 		CALL DWORD PTR[eax + 16]
-		JMP in_packet_hook_return
+		JMP packet_in_hook_return
 	}
 }
 
-void IdleLoop()
+const char* print_out_format1 = "[OT] %.*s\n";
+void __declspec(naked) packet_out_hook1()
+{
+	__asm
+	{
+		// +13
+
+		PUSHAD
+		MOV eax, [edi]
+		ADD eax, 13
+		PUSH eax // str
+		MOV ecx, [edi + 4]
+		SUB ecx, 13
+		PUSH ecx // len
+		PUSH print_out_format1
+		CALL printf
+		ADD esp, 12
+		POPAD
+
+		// overwritten
+		CMP DWORD PTR[ebp + 16], 1
+		SETZ BYTE PTR [ebp+4]
+		JMP packet_out_hook_return
+	}
+}
+
+void idle_loop()
 {
 	while (true)
 	{
--- TS3Hook/main.h
+++ TS3Hook/main.h
@@ -9,15 +9,19 @@
 #include <windows.h>
 
 // CONFIG
-const char* MASK1 = "\x8B\x4F\x3C\x6A\x00\xFF\x77\x44\xFF\x77\x40\x8B\x01\x57\x56\xFF\x50\x10";
-const char* PATT1 = "xxxxxxxxxxxxxxxxxx";
-const DWORD OFFS1 = 13;
+const char* MASK_IN_1 = "\x8B\x4F\x3C\x6A\x00\xFF\x77\x44\xFF\x77\x40\x8B\x01\x57\x56\xFF\x50\x10";
+const char* PATT_IN_1 = "xxxxxxxxxxxxxxxxxx";
+
+const char* MASK_OUT_1 = "\xC6\x45\xFC\x06\x80\xF9\x02\x74\x09\x80\xF9\x03";
+const char* PATT_OUT_1 = "xxxxxxxxxxxx";
 
 // RUNTIME CALCED
-DWORD in_packet_hook_return = 0x0;
+DWORD packet_in_hook_return = 0x0;
+DWORD packet_out_hook_return = 0x0;
 
 // FUNCTION DECLS
-void IdleLoop();
+void idle_loop();
 
-void in_packet_hook();
+void packet_in_hook1();
+void packet_out_hook1();
 #endif
